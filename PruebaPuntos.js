ae628d65700fda3f2cdf5a3fedcdc99d5ff4603e
NodoRiesgo <35318896+NodoRiesgo@users.noreply.github.com>
2019-01-16 02:16:47 -0300

    Add files via upload

---
+++ PruebaPuntos.js
@@ -0,0 +1,12 @@
+var PruebaPuntos = {
+"type": "FeatureCollection",
+"name": "xPrueba SHP Puntos",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:EPSG::32719" } },
+"features": [
+{ "type": "Feature", "properties": { "CompaÃ±ia": "Quinta de Chimbarongo", "Direccion": "Jose Miguel Carrera S\/N", "Telefono": "(72) 781150", "NOMBRE": "Chimbarongo", "NOMBRE_1": "Libertador Gral. Bernardo O'Higgins" }, "geometry": { "type": "Point", "coordinates": [ 321726.017699999734759, 6149704.9824 ] } },
+{ "type": "Feature", "properties": { "CompaÃ±ia": "Tercera de Chimbarongo", "Direccion": "Carmen Larrain 13", "Telefono": "(72) 781148", "NOMBRE": "Chimbarongo", "NOMBRE_1": "Libertador Gral. Bernardo O'Higgins" }, "geometry": { "type": "Point", "coordinates": [ 313058.074199999682605, 6157350.770299999975 ] } },
+{ "type": "Feature", "properties": { "CompaÃ±ia": "Segunda de Chimbarongo", "Direccion": "Carmen Larrain 13", "Telefono": "(72) 781147", "NOMBRE": "Chimbarongo", "NOMBRE_1": "Libertador Gral. Bernardo O'Higgins" }, "geometry": { "type": "Point", "coordinates": [ 312985.571600000374019, 6157396.9083 ] } },
+{ "type": "Feature", "properties": { "CompaÃ±ia": "Primera de Chimbarongo", "Direccion": "Carmen Larrain 13", "Telefono": "(72) 781146", "NOMBRE": "Chimbarongo", "NOMBRE_1": "Libertador Gral. Bernardo O'Higgins" }, "geometry": { "type": "Point", "coordinates": [ 312915.949799999594688, 6157438.466599999927 ] } },
+{ "type": "Feature", "properties": { "CompaÃ±ia": "Cuarta de Chimbarongo", "Direccion": "Las Quezadas S\/N", "Telefono": "(72) 781149", "NOMBRE": "Chimbarongo", "NOMBRE_1": "Libertador Gral. Bernardo O'Higgins" }, "geometry": { "type": "Point", "coordinates": [ 318209.629099999554455, 6165680.660799999721 ] } }
+]
+}
